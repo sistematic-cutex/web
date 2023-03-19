c11f6966d402b685c586318cf91f4520d4405a5c
Katerine Morcillo <katerine.morcillo@misena.edu.co>
2023-03-19 18:26:52 -0500

    Creación de tabla providers, migración y conección a base de datos

---
+++ database/migrations/2023_03_19_225249_create_providers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('providers', function (Blueprint $table) {
+            $table->id();
+            $table->string('business_name', 125);
+            $table->string('admin_name', 125);
+            $table->string('cellphone', 20);
+            $table->string('email', 125);
+            $table->string('address', 125);
+            $table->enum('status', ["active", "inactive"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('providers');
+    }
+};
