df550275acc98417fb228849dc34e4fb4cee3fc3
brandonrosas56 <brayanrosas635gmail.com>
2023-03-22 21:02:32 -0500

    migracion company y modelo

---
+++ database/migrations/2023_03_22_190744_create_companies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('nit', 20);
+            $table->string('owner_name', 45);
+            $table->date('date');
+            $table->string('address', 45);
+            $table->string('email', 45);
+            $table->string('cellphone', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
